d0996a92fb1f59fc6e856555efda98707121e190
AristoteLeDaron <aristotenataf@gmail.com>
2023-10-27 15:47:01 +0200

    ajout de la descriptionbox et de son css

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+
+const DescriptionBox = () => {
+  return (
+    <div className="descriptionbox">
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus exercitationem rem error ullam cum modi consequatur corporis, blanditiis obcaecati alias maxime, excepturi, at nulla. Iure perspiciatis repudiandae provident alias ipsum.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, doloribus adipisci optio alias rem sit eum debitis est repellendus autem praesentium quis molestiae quod animi officia sequi veritatis, quidem aspernatur!</p>
+      </div>
+
+    </div>
+  )
+}
+
+export default DescriptionBox
