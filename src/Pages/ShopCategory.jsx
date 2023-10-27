e59f8c7bc830c4e20ec771e6981c6d2f77a5bc73
AristoteLeDaron <aristotenataf@gmail.com>
2023-10-27 04:26:32 +0200

    css de la page de category de produits

--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -3,11 +3,12 @@ import './CSS/ShopCategory.css'
 import { ShopContext } from '../Context/ShopContext'
 import dropdown_icon from '../Components/Assets/dropdown_icon.png'
 import Item from '../Components/Item/Item'
+
 const ShopCategory = (props) => {
   const { all_product } = useContext(ShopContext);
   return (
     <div className="shop-category">
-      <img src={props.banner} alt="" />
+      <img className='shopcategory-banner' src={props.banner} alt="" />
       <div className="shopcategory-indexSort">
         <p>
           <span>Showing 1-12</span>out of 36 products
@@ -26,6 +27,9 @@ const ShopCategory = (props) => {
           }
 
         })}
+      </div>
+      <div className="shopcategory-loadmore">
+        Explore More
       </div>
 
     </div>
