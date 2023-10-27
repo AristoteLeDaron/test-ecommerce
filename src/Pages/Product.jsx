a43e3c2b2441bf151f2edf0ddb84dcbf65630a52
AristoteLeDaron <aristotenataf@gmail.com>
2023-10-27 15:13:42 +0200

    ajout de ProductDisplay.jsx et de son css Ajout du css de breadcrum

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -3,6 +3,7 @@ import './CSS/Product.css'
 import { ShopContext } from '../Context/ShopContext';
 import { useParams } from 'react-router-dom';
 import Breadcrum from '../Components/Breadcrums/Breadcrum';
+import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
 
 
 const Product = () => {
@@ -15,6 +16,7 @@ const Product = () => {
   return (
     <div>
       <Breadcrum product={product} />
+      <ProductDisplay product={product} />
 
     </div>
 
